ecd01e360040942e318af70b3eb19592769dbb35
sabrinna <146647799+alqsdeuss@users.noreply.github.com>
2025-10-10 02:16:36 +0300

    Create get.js

---
+++ get.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        document.getElementById('load').classList.add('hide');
+        document.getElementById('main').classList.add('show');
+    }, 2500);
+});
